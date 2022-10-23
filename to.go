6c8f5228d39b4a0d7e3bac27e95dc7258fe90844
CC11001100 <CC11001100@qq.com>
2022-10-24 02:00:26 +0800

    feat: add some to

--- to.go
+++ to.go
@@ -5,9 +5,35 @@ func To[From, To any](slice []From, toFunc func(item From) To) []To {
 	return Map[From, To](slice, toFunc)
 }
 
-func ToMap[From, To any](slice []From) {}
+// ToMap 把切片转为map
+func ToMap[T, K comparable, V any](slice []T, toMapFunc func(index int, item T) (K, V)) map[K]V {
+	resultMap := make(map[K]V, 0)
+	for index, item := range slice {
+		k, v := toMapFunc(index, item)
+		resultMap[k] = v
+	}
+	return resultMap
+}
 
-func ToSet() {}
+// ToSet 把切片转为set
+func ToSet[T any, S comparable](slice []T, toSetFunc func(index int, item T) S) map[S]struct{} {
+	set := make(map[S]struct{}, 0)
+	for index, item := range slice {
+		set[toSetFunc(index, item)] = struct{}{}
+	}
+	return set
+}
+
+// Flat2 二维切片打平为一维切片
+func Flat2[T any](slice [][]T) []T {
+	resultSlice := make([]T, 0)
+	for _, itemSlice := range slice {
+		for _, item := range itemSlice {
+			resultSlice = append(resultSlice, item)
+		}
+	}
+	return resultSlice
+}
 
 // 多维数组转为一维数组
 
