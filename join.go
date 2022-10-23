e9f31ea4d1f72c510745f1b930f81e4a3213c48a
CC11001100 <CC11001100@qq.com>
2022-10-24 02:37:06 +0800

    feat:  join

--- join.go
+++ join.go
@@ -1,25 +1,106 @@
 package slice
 
+type KeyFunc[T any, K comparable] func(index int, item T) K
+
+// Join 两个切片join，两个都有的才会被join返回
 func Join[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
-	return nil
+	sliceAMap := make(map[K]T, 0)
+	for indexA, itemA := range sliceA {
+		keyA := keyFunc(indexA, itemA)
+		sliceAMap[keyA] = itemA
+	}
+	sliceADistinctSet := make(map[K]struct{}, 0)
+	joinResultMap := make(map[K][]T, 0)
+	for indexB, itemB := range sliceB {
+		keyB := keyFunc(indexB, itemB)
+		if itemA, exists := sliceAMap[keyB]; exists {
+			if _, exists := sliceADistinctSet[keyB]; !exists {
+				joinResultMap[keyB] = append(joinResultMap[keyB], itemA)
+				sliceADistinctSet[keyB] = struct{}{}
+			}
+			joinResultMap[keyB] = append(joinResultMap[keyB], itemB)
+		}
+	}
+	return joinResultMap
 }
 
-// countByKey
+// LeftJoin 左边全来
+func LeftJoin[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
+	sliceAMap := make(map[K]T, 0)
+	for indexA, itemA := range sliceA {
+		keyA := keyFunc(indexA, itemA)
+		sliceAMap[keyA] = itemA
+	}
+	sliceADistinctSet := make(map[K]struct{}, 0)
+	joinResultMap := make(map[K][]T, 0)
+	for indexB, itemB := range sliceB {
+		keyB := keyFunc(indexB, itemB)
+		if itemA, exists := sliceAMap[keyB]; exists {
+			if _, exists := sliceADistinctSet[keyB]; !exists {
+				joinResultMap[keyB] = append(joinResultMap[keyB], itemA)
+				sliceADistinctSet[keyB] = struct{}{}
+			}
+			joinResultMap[keyB] = append(joinResultMap[keyB], itemB)
+		}
+	}
 
-type KeyFunc[T any, K comparable] func(item T) K
+	// 把没join上的sliceA的元素也加入到结果
+	for index, itemA := range sliceA {
+		keyA := keyFunc(index, itemA)
+		if _, exists := sliceADistinctSet[keyA]; exists {
+			continue
+		}
+		joinResultMap[keyA] = append(joinResultMap[keyA], itemA)
+		sliceADistinctSet[keyA] = struct{}{}
+	}
 
-func CoGroup[T any, K string | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](keyFunc KeyFunc[T, K], slices ...[]T) map[K][]T {
+	return joinResultMap
+}
+
+// RightJoin 右边全来
+func RightJoin[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
+	return LeftJoin(sliceB, sliceA, keyFunc)
+}
+
+// FullJoin 两边全来
+func FullJoin[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
+	joinResultMap := make(map[K][]T, 0)
+	for indexA, itemA := range sliceA {
+		keyA := keyFunc(indexA, itemA)
+		joinResultMap[keyA] = append(joinResultMap[keyA], itemA)
+	}
+	for indexB, itemB := range sliceB {
+		keyB := keyFunc(indexB, itemB)
+		joinResultMap[keyB] = append(joinResultMap[keyB], itemB)
+	}
+	return joinResultMap
+}
+
+func CoGroup[T any, K comparable](keyFunc KeyFunc[T, K], slices ...[]T) map[K][]T {
 	resultMap := make(map[K][]T, 0)
 	for _, slice := range slices {
-		for _, item := range slice {
-			key := keyFunc(item)
+		for index, item := range slice {
+			key := keyFunc(index, item)
 			resultMap[key] = append(resultMap[key], item)
 		}
 	}
 	return resultMap
 }
 
-// FoldByKey 根据key折叠合并元素
+// FoldByKey 根据key折叠合并切片中的元素
 func FoldByKey[T any, K comparable](slice []T, keyFunc KeyFunc[T, K], mergeFunc func(a T, b T) T) []T {
-	return nil
+	keyMap := make(map[K]T, 0)
+	for index, item := range slice {
+		key := keyFunc(index, item)
+		if lastItem, exists := keyMap[key]; exists {
+			keyMap[key] = mergeFunc(lastItem, item)
+		} else {
+			keyMap[key] = item
+		}
+	}
+	newSlice := make([]T, 0)
+	for _, item := range keyMap {
+		newSlice = append(newSlice, item)
+	}
+	return newSlice
 }
