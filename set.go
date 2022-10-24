ca60ccd3e724cd432bd749853cac55e4cac7a483
CC11001100 <CC11001100@qq.com>
2022-10-25 02:07:59 +0800

    feat: prefect set.go

--- set.go
+++ set.go
@@ -1,5 +1,10 @@
 package slice
 
-// 如果有这个下标存在的话则设置值
-
-
+// Set 如果有这个下标存在的话则设置值，返回是否设置成功
+func Set[T any](slice []T, index int, value T) bool {
+	if index >= len(slice) {
+		return false
+	}
+	slice[index] = value
+	return true
+}
