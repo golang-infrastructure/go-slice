655d43d4e17b9ac9b233a4065d54baa710074cf1
CC11001100 <CC11001100@qq.com>
2022-10-26 00:37:54 +0800

    from.go & from_test.go

--- from.go
+++ from.go
@@ -1,22 +1,25 @@
 package slice
 
-//import "reflect"
-//
+// TODO 需要思考下是否要加入对这个方法的支持，会有什么实际场景用得到吗？
 //func New[T any](itemType reflect.Type) []T {
 //
 //}
 
+// ---------------------------------------------------------------------------------------------------------------------
+
 // FromItems 从一个或多个元素创建切片
 func FromItems[T any](items ...T) []T {
-	slice := make([]T, len(items), len(items))
+	slice := make([]T, len(items))
 	for index, item := range items {
 		slice[index] = item
 	}
 	return slice
 }
 
-// FromMap 从map创建一个切片
-func FromMap[K comparable, V, T any](fromMap map[K]V, fromMapFunc func(key K, value V) T) []T {
+// ---------------------------------------------------------------------------------------------------------------------
+
+// FromMapByFunc 从map创建一个切片，使用自定义的函数从map中抽取值
+func FromMapByFunc[K comparable, V, T any](fromMap map[K]V, fromMapFunc func(key K, value V) T) []T {
 	slice := make([]T, 0)
 	for key, value := range fromMap {
 		slice = append(slice, fromMapFunc(key, value))
@@ -24,8 +27,8 @@ func FromMap[K comparable, V, T any](fromMap map[K]V, fromMapFunc func(key K, va
 	return slice
 }
 
-// FromMapFlat 从map创建一个切片，map的一个键值对可能会有多个返回值，这多个返回会被打平
-func FromMapFlat[K comparable, V, T any](fromMap map[K]V, fromMapFunc func(key K, value V) []T) []T {
+// FromMapFlatByFunc 从map创建一个切片，map的一个键值对可能会有多个返回值，这多个返回会被打平
+func FromMapFlatByFunc[K comparable, V, T any](fromMap map[K]V, fromMapFunc func(key K, value V) []T) []T {
 	slice := make([]T, 0)
 	for key, value := range fromMap {
 		slice = append(slice, fromMapFunc(key, value)...)
@@ -33,8 +36,37 @@ func FromMapFlat[K comparable, V, T any](fromMap map[K]V, fromMapFunc func(key K
 	return slice
 }
 
-// FromSet 从set创建一个切片
-func FromSet[K comparable, T any](set map[K]struct{}, fromSetFunc func(key K) T) []T {
+// FromMapKey 把map的key转为切片
+func FromMapKey[K comparable, V any](fromMap map[K]V) []K {
+	slice := make([]K, 0)
+	for key := range fromMap {
+		slice = append(slice, key)
+	}
+	return slice
+}
+
+// FromMapValue 把map的value转为切片
+func FromMapValue[K comparable, V any](fromMap map[K]V) []V {
+	slice := make([]V, 0)
+	for _, value := range fromMap {
+		slice = append(slice, value)
+	}
+	return slice
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// FromSet 把se转换为切片
+func FromSet[K comparable](set map[K]struct{}) []K {
+	slice := make([]K, 0)
+	for key := range set {
+		slice = append(slice, key)
+	}
+	return slice
+}
+
+// FromSetByFunc 从set创建一个切片
+func FromSetByFunc[K comparable, T any](set map[K]struct{}, fromSetFunc func(key K) T) []T {
 	slice := make([]T, 0)
 	for key := range set {
 		slice = append(slice, fromSetFunc(key))
@@ -42,11 +74,13 @@ func FromSet[K comparable, T any](set map[K]struct{}, fromSetFunc func(key K) T)
 	return slice
 }
 
-// FromSetFlat 从set创建一个切片，set的一个键可能会有多个返回值，这多个返回会被打平
-func FromSetFlat[K comparable, T any](set map[K]struct{}, fromSetFunc func(key K) []T) []T {
+// FromSetFlatByFunc 从set创建一个切片，set的一个键可能会有多个返回值，这多个返回会被打平
+func FromSetFlatByFunc[K comparable, T any](set map[K]struct{}, fromSetFunc func(key K) []T) []T {
 	slice := make([]T, 0)
 	for key := range set {
 		slice = append(slice, fromSetFunc(key)...)
 	}
 	return slice
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
---
+++ from_test.go
@@ -0,0 +1,94 @@
+package slice
+
+import (
+	"testing"
+)
+
+func TestFromItems(t *testing.T) {
+	t.Log(FromItems(1, 2, 3))
+}
+
+func TestFromMapByFunc(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+	}
+	slice := FromMapByFunc(a, func(key string, value int) int {
+		return value + 1
+	})
+	t.Log(slice)
+}
+
+func TestFromMapFlatByFunc(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+	}
+	slice := FromMapFlatByFunc(a, func(key string, value int) []int {
+		return []int{value, value + 1}
+	})
+	t.Log(slice)
+}
+
+func TestFromMapKey(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+	}
+	slice := FromMapKey(a)
+	t.Log(slice)
+}
+
+func TestFromMapValue(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+	}
+	slice := FromMapValue(a)
+	t.Log(slice)
+}
+
+func TestFromSet(t *testing.T) {
+	a := map[string]struct{}{
+		"a": struct{}{},
+		"b": struct{}{},
+		"c": struct{}{},
+		"d": struct{}{},
+	}
+	slice := FromSet(a)
+	t.Log(slice)
+}
+
+func TestFromSetByFunc(t *testing.T) {
+	a := map[string]struct{}{
+		"a": struct{}{},
+		"b": struct{}{},
+		"c": struct{}{},
+		"d": struct{}{},
+	}
+	slice := FromSetByFunc(a, func(key string) string {
+		return key + "aaaa"
+	})
+	t.Log(slice)
+}
+
+func TestFromSetFlatByFunc(t *testing.T) {
+	a := map[string]struct{}{
+		"a": struct{}{},
+		"b": struct{}{},
+		"c": struct{}{},
+		"d": struct{}{},
+	}
+	slice := FromSetFlatByFunc(a, func(key string) []string {
+		return []string{key, key + "aaaa"}
+	})
+	t.Log(slice)
+}
