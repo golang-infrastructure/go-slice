847edfff13696182748446b6ff2b4552d6abe058
CC11001100 <CC11001100@qq.com>
2022-10-25 23:58:39 +0800

    完善choose.go

--- choose.go
+++ choose.go
@@ -2,17 +2,25 @@ package slice
 
 import "math/rand"
 
+// 这个文件中的方法应该都是下标安全的
+
 // ---------------------------------------------------------------------------------------------------------------------
 
 // SubSlice 子切片，不使用指针以防止引用释放不掉
 func SubSlice[T any](slice []T, from, to int) []T {
-	if to <= from || to < 0 || from < 0 {
+	if from < 0 {
+		from = 0
+	}
+	if to > len(slice) {
+		to = len(slice)
+	}
+	if to <= from || len(slice) == 0 {
 		return nil
 	}
 	capacity := to - from
-	subSlice := make([]T, 0, capacity)
+	subSlice := make([]T, capacity, capacity)
 	for i := from; i < to && i < len(slice); i++ {
-		subSlice[i-from] = slice[from]
+		subSlice[i-from] = slice[i]
 	}
 	return subSlice
 }
@@ -30,11 +38,13 @@ func Filter[T any](slice []T, filterFunc func(item T) bool) []T {
 
 // ------------------------------------------------ By Index ---------------------------------------------------------------------
 
+// ChooseIndex 返回切片中给定下表的元素，如果不存在的话返回对应类型的零值
 func ChooseIndex[T any](slice []T, index int) T {
 	var zero T
 	return ChooseIndexOrDefault(slice, index, zero)
 }
 
+// ChooseIndexOrDefault 返回切片中给定下表的元素，如果不存在的话返回给定的默认值
 func ChooseIndexOrDefault[T any](slice []T, index int, defaultValue T) T {
 	if index < 0 || index >= len(slice) {
 		return defaultValue
@@ -42,21 +52,14 @@ func ChooseIndexOrDefault[T any](slice []T, index int, defaultValue T) T {
 	return slice[index]
 }
 
-// ChooseIndexes 选择给定的下标的元素
+// ChooseIndexes 返回给定的几个下标的元素，如果越界的话对应位置会填充为零值，会保证返回的切片的长度等于给的索引的数量
 func ChooseIndexes[T any](slice []T, indexes ...int) []T {
-
-	// indexes --> set
-	indexSet := make(map[int]struct{}, 0)
-	for _, index := range indexes {
-		indexSet[index] = struct{}{}
+	if len(indexes) == 0 {
+		return nil
 	}
-
-	// filter
-	newSlice := make([]T, 0)
-	for index, item := range slice {
-		if _, exists := indexSet[index]; exists {
-			newSlice = append(newSlice, item)
-		}
+	newSlice := make([]T, len(indexes), len(indexes))
+	for newSliceIndex, index := range indexes {
+		newSlice[newSliceIndex] = ChooseIndex(slice, index)
 	}
 	return newSlice
 }
@@ -96,7 +99,7 @@ func ChooseRandomIndex[T any](slice []T) T {
 
 // ChooseRandomIndexes 从切片中随机选择n个元素，会避免重复选择
 func ChooseRandomIndexes[T any](slice []T, n int) []T {
-	if n == 0 {
+	if n == 0 || len(slice) == 0 {
 		return nil
 	}
 	// 抖个机灵，借助于map访问时的随机性来实现随机选择
@@ -143,11 +146,14 @@ func FirstItemOrDefault[T any](slice []T, defaultValue T) T {
 
 // FirstItems 选择切片最前面几个元素
 func FirstItems[T any](slice []T, n int) []T {
-	newSlice := make([]T, 0)
-	for i := 0; i < n && i < len(slice); i++ {
-		newSlice = append(newSlice, slice[i])
+	if n <= 0 {
+		return nil
 	}
-	return newSlice
+	// 避免越界，如果选多了的话就选到头就算完
+	if n > len(slice) {
+		n = len(slice)
+	}
+	return slice[0:n]
 }
 
 // LastItem 返回切片的最后一个元素，如果有的话
@@ -167,15 +173,13 @@ func LastItemOrDefault[T any](slice []T, defaultValue T) T {
 
 // LastItems 选择切片的最后几个元素，会保持其顺序
 func LastItems[T any](slice []T, n int) []T {
-	newSlice := make([]T, 0)
-	beginIndex := len(slice) - n - 1
-	if beginIndex < 0 {
-		beginIndex = 0
+	if n <= 0 {
+		return nil
+	} else if n >= len(slice) {
+		return slice
+	} else {
+		return slice[len(slice)-n:]
 	}
-	for ; beginIndex < len(slice); beginIndex++ {
-		newSlice = append(newSlice, slice[beginIndex])
-	}
-	return newSlice
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
---
+++ choose_test.go
@@ -0,0 +1,106 @@
+package slice
+
+import (
+	"testing"
+)
+
+func TestChooseEvenIndexes(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	t.Log(ChooseEvenIndexes(slice))
+}
+
+func TestChooseIndex(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	t.Log(ChooseIndex(slice, 1))
+	t.Log(ChooseIndex(slice, 100))
+}
+
+func TestChooseIndexOrDefault(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	t.Log(ChooseIndexOrDefault(slice, 1, 10))
+	t.Log(ChooseIndexOrDefault(slice, 100, 100))
+}
+
+func TestChooseIndexes(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	t.Log(ChooseIndexes(slice))
+	t.Log(ChooseIndexes(slice, 1, 100))
+}
+
+func TestChooseMiddleIndex(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	t.Log(ChooseMiddleIndex(slice))
+}
+
+func TestChooseOddIndexes(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5}
+	t.Log(ChooseOddIndexes(slice))
+}
+
+func TestChooseRandomIndex(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(ChooseRandomIndex(slice))
+}
+
+func TestChooseRandomIndexes(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(ChooseRandomIndexes(slice, 1))
+	t.Log(ChooseRandomIndexes(slice, 4))
+	t.Log(ChooseRandomIndexes(slice, 100))
+	t.Log(ChooseRandomIndexes(slice, 0))
+}
+
+func TestFilter(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(Filter(slice, func(item int) bool {
+		return item%2 == 0
+	}))
+}
+
+func TestFirstItem(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(FirstItem(slice))
+}
+
+func TestFirstItemOrDefault(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(FirstItemOrDefault(slice, 1))
+	t.Log(FirstItemOrDefault(slice, 0))
+	t.Log(FirstItemOrDefault(slice, -1))
+}
+
+func TestFirstItems(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(FirstItems(slice, 1))
+	t.Log(FirstItems(slice, 0))
+	t.Log(FirstItems(slice, -1))
+}
+
+func TestLastItem(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(LastItem(slice))
+	var s []int
+	t.Log(LastItem(s))
+	var s2 []any
+	t.Log(LastItem(s2))
+}
+
+func TestLastItemOrDefault(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(LastItemOrDefault(slice, 1))
+	var s []int
+	t.Log(LastItemOrDefault(s, 1))
+}
+
+func TestLastItems(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(LastItems(slice, 3))
+	t.Log(LastItems(slice, 0))
+	t.Log(LastItems(slice, -1))
+}
+
+func TestSubSlice(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	t.Log(SubSlice(slice, 0, 3))
+	t.Log(SubSlice(slice, -1, 10000))
+}
