77f8383f100953f67c26466d13cf7c9213a91503
CC11001100 <CC11001100@qq.com>
2022-10-26 03:00:16 +0800

    完善join.go

--- join.go
+++ join.go
@@ -1,97 +1,182 @@
 package slice
 
-// JoinSlice 两个切片join，两个都有的才会被join返回
-func JoinSlice[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
-	sliceAMap := make(map[K]T, 0)
+import "github.com/CC11001100/go-tuple"
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// JoinByKeyFunc 两个切片join，两个都有的才会被join返回，注意如果数据有重复会笛卡尔积，与数据库的join行为保持一致
+func JoinByKeyFunc[A, B any, K comparable](sliceA []A, keyFuncA KeyFunc[A, K], sliceB []B, keyFuncB KeyFunc[B, K]) map[K][]*tuple.Tuple2[A, B] {
+	// 先把A做成一个关联点，因为可能会存在多个相同key的元素，要都能够保留着
+	sliceAMap := make(map[K][]A, 0)
 	for indexA, itemA := range sliceA {
-		keyA := keyFunc(indexA, itemA)
-		sliceAMap[keyA] = itemA
+		keyA := keyFuncA(indexA, itemA)
+		sliceAMap[keyA] = append(sliceAMap[keyA], itemA)
 	}
-	sliceADistinctSet := make(map[K]struct{}, 0)
-	joinResultMap := make(map[K][]T, 0)
+	joinResultMap := make(map[K][]*tuple.Tuple2[A, B], 0)
 	for indexB, itemB := range sliceB {
-		keyB := keyFunc(indexB, itemB)
-		if itemA, exists := sliceAMap[keyB]; exists {
-			if _, exists := sliceADistinctSet[keyB]; !exists {
-				joinResultMap[keyB] = append(joinResultMap[keyB], itemA)
-				sliceADistinctSet[keyB] = struct{}{}
+		keyB := keyFuncB(indexB, itemB)
+		if itemASlice, exists := sliceAMap[keyB]; exists {
+			for _, itemA := range itemASlice {
+				joinResultMap[keyB] = append(joinResultMap[keyB], tuple.New2(itemA, itemB))
 			}
-			joinResultMap[keyB] = append(joinResultMap[keyB], itemB)
 		}
 	}
 	return joinResultMap
 }
 
-// LeftJoinSlice 左边全来
-func LeftJoinSlice[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
-	sliceAMap := make(map[K]T, 0)
+// LeftJoinByKeyFunc 左边全来
+func LeftJoinByKeyFunc[A, B any, K comparable](sliceA []A, keyFuncA KeyFunc[A, K], sliceB []B, keyFuncB KeyFunc[B, K]) map[K][]*tuple.Tuple2[A, B] {
+
+	// 除了保留切片A的元素，还要把下表也保留着，等下要把被join成功的下标都收集得到
+	sliceAMap := make(map[K][]*tuple.Tuple2[A, int], 0)
 	for indexA, itemA := range sliceA {
-		keyA := keyFunc(indexA, itemA)
-		sliceAMap[keyA] = itemA
+		keyA := keyFuncA(indexA, itemA)
+		sliceAMap[keyA] = append(sliceAMap[keyA], tuple.New2(itemA, indexA))
 	}
-	sliceADistinctSet := make(map[K]struct{}, 0)
-	joinResultMap := make(map[K][]T, 0)
+	sliceAJoinSuccessIndexSet := make(map[int]struct{}, 0)
+	joinResultMap := make(map[K][]*tuple.Tuple2[A, B], 0)
 	for indexB, itemB := range sliceB {
-		keyB := keyFunc(indexB, itemB)
-		if itemA, exists := sliceAMap[keyB]; exists {
-			if _, exists := sliceADistinctSet[keyB]; !exists {
-				joinResultMap[keyB] = append(joinResultMap[keyB], itemA)
-				sliceADistinctSet[keyB] = struct{}{}
+		keyB := keyFuncB(indexB, itemB)
+		if itemASlice, exists := sliceAMap[keyB]; exists {
+			for _, itemA := range itemASlice {
+				joinResultMap[keyB] = append(joinResultMap[keyB], tuple.New2(itemA.V1, itemB))
+				sliceAJoinSuccessIndexSet[itemA.V2] = struct{}{}
 			}
-			joinResultMap[keyB] = append(joinResultMap[keyB], itemB)
 		}
 	}
 
 	// 把没join上的sliceA的元素也加入到结果
 	for index, itemA := range sliceA {
-		keyA := keyFunc(index, itemA)
-		if _, exists := sliceADistinctSet[keyA]; exists {
+		if _, exists := sliceAJoinSuccessIndexSet[index]; exists {
 			continue
 		}
-		joinResultMap[keyA] = append(joinResultMap[keyA], itemA)
-		sliceADistinctSet[keyA] = struct{}{}
+		keyA := keyFuncA(index, itemA)
+		var zero B
+		joinResultMap[keyA] = append(joinResultMap[keyA], tuple.New2(itemA, zero))
 	}
 
 	return joinResultMap
 }
 
-// RightJoinSlice 右边全来
-func RightJoinSlice[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
-	return LeftJoinSlice(sliceB, sliceA, keyFunc)
-}
-
-// FullJoinSlice 两边全来
-func FullJoinSlice[T any, K comparable](sliceA []T, sliceB []T, keyFunc KeyFunc[T, K]) map[K][]T {
-	joinResultMap := make(map[K][]T, 0)
-	for indexA, itemA := range sliceA {
-		keyA := keyFunc(indexA, itemA)
-		joinResultMap[keyA] = append(joinResultMap[keyA], itemA)
-	}
+// RightJoinByKeyFunc 右边全来
+func RightJoinByKeyFunc[A, B any, K comparable](sliceA []A, keyFuncA KeyFunc[A, K], sliceB []B, keyFuncB KeyFunc[B, K]) map[K][]*tuple.Tuple2[A, B] {
+	// 除了保留切片A的元素，还要把下表也保留着，等下要把被join成功的下标都收集得到
+	sliceBMap := make(map[K][]*tuple.Tuple2[B, int], 0)
 	for indexB, itemB := range sliceB {
-		keyB := keyFunc(indexB, itemB)
-		joinResultMap[keyB] = append(joinResultMap[keyB], itemB)
+		keyB := keyFuncB(indexB, itemB)
+		sliceBMap[keyB] = append(sliceBMap[keyB], &tuple.Tuple2[B, int]{itemB, indexB})
 	}
-	return joinResultMap
-}
-
-func CoGroup[T any, K comparable](keyFunc KeyFunc[T, K], slices ...[]T) map[K][]T {
-	resultMap := make(map[K][]T, 0)
-	for _, slice := range slices {
-		for index, item := range slice {
-			key := keyFunc(index, item)
-			resultMap[key] = append(resultMap[key], item)
+	sliceBJoinSuccessIndexSet := make(map[int]struct{}, 0)
+	joinResultMap := make(map[K][]*tuple.Tuple2[A, B], 0)
+	for indexA, itemA := range sliceA {
+		keyA := keyFuncA(indexA, itemA)
+		if itemBSlice, exists := sliceBMap[keyA]; exists {
+			for _, itemB := range itemBSlice {
+				joinResultMap[keyA] = append(joinResultMap[keyA], &tuple.Tuple2[A, B]{itemA, itemB.V1})
+				sliceBJoinSuccessIndexSet[itemB.V2] = struct{}{}
+			}
 		}
 	}
+
+	// 把没join上的sliceB的元素也加入到结果
+	for index, itemB := range sliceB {
+		if _, exists := sliceBJoinSuccessIndexSet[index]; exists {
+			continue
+		}
+		keyB := keyFuncB(index, itemB)
+		var zero A
+		joinResultMap[keyB] = append(joinResultMap[keyB], &tuple.Tuple2[A, B]{zero, itemB})
+	}
+
+	return joinResultMap
+}
+
+// FullJoinByKeyFunc 两边全来
+func FullJoinByKeyFunc[A, B any, K comparable](sliceA []A, keyFuncA KeyFunc[A, K], sliceB []B, keyFuncB KeyFunc[B, K]) map[K][]*tuple.Tuple2[A, B] {
+
+	// 除了保留切片A的元素，还要把下表也保留着，等下要把被join成功的下标都收集得到
+	sliceAMap := make(map[K][]*tuple.Tuple2[A, int], 0)
+	for indexA, itemA := range sliceA {
+		keyA := keyFuncA(indexA, itemA)
+		sliceAMap[keyA] = append(sliceAMap[keyA], &tuple.Tuple2[A, int]{itemA, indexA})
+	}
+	sliceAJoinSuccessIndexSet := make(map[int]struct{}, 0)
+	sliceBJoinSuccessIndexSet := make(map[int]struct{}, 0)
+	joinResultMap := make(map[K][]*tuple.Tuple2[A, B], 0)
+	for indexB, itemB := range sliceB {
+		keyB := keyFuncB(indexB, itemB)
+		if itemASlice, exists := sliceAMap[keyB]; exists {
+			for _, itemA := range itemASlice {
+				joinResultMap[keyB] = append(joinResultMap[keyB], &tuple.Tuple2[A, B]{itemA.V1, itemB})
+				sliceAJoinSuccessIndexSet[itemA.V2] = struct{}{}
+				sliceBJoinSuccessIndexSet[indexB] = struct{}{}
+			}
+		}
+	}
+
+	// 把没join上的sliceA的元素也加入到结果
+	for index, itemA := range sliceA {
+		if _, exists := sliceAJoinSuccessIndexSet[index]; exists {
+			continue
+		}
+		keyA := keyFuncA(index, itemA)
+		var zero B
+		joinResultMap[keyA] = append(joinResultMap[keyA], &tuple.Tuple2[A, B]{itemA, zero})
+	}
+
+	// 把没join上的sliceB的元素也加入到结果
+	for index, itemB := range sliceB {
+		if _, exists := sliceBJoinSuccessIndexSet[index]; exists {
+			continue
+		}
+		keyB := keyFuncB(index, itemB)
+		var zero A
+		joinResultMap[keyB] = append(joinResultMap[keyB], &tuple.Tuple2[A, B]{zero, itemB})
+	}
+
+	return joinResultMap
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// CoGroup 将多个切片中的元素以key做关联，相同的key会被放到同一个key下的切片中
+func CoGroup[A, B any, K comparable](sliceA []A, keyFuncA KeyFunc[A, K], sliceB []B, keyFuncB KeyFunc[B, K]) map[K]*tuple.Tuple2[[]A, []B] {
+	resultMap := make(map[K]*tuple.Tuple2[[]A, []B], 0)
+
+	// 切片A
+	for indexA, itemA := range sliceA {
+		keyA := keyFuncA(indexA, itemA)
+		if _, exists := resultMap[keyA]; !exists {
+			resultA := make([]A, 0)
+			resultB := make([]B, 0)
+			resultMap[keyA] = tuple.New2[[]A, []B](resultA, resultB)
+		}
+		resultMap[keyA].V1 = append(resultMap[keyA].V1, itemA)
+	}
+
+	// 切片B
+	for indexB, itemB := range sliceB {
+		keyA := keyFuncB(indexB, itemB)
+		if _, exists := resultMap[keyA]; !exists {
+			resultA := make([]A, 0)
+			resultB := make([]B, 0)
+			resultMap[keyA] = tuple.New2[[]A, []B](resultA, resultB)
+		}
+		resultMap[keyA].V2 = append(resultMap[keyA].V2, itemB)
+	}
+
 	return resultMap
 }
 
-// FoldByKey 根据key折叠合并切片中的元素
-func FoldByKey[T any, K comparable](slice []T, keyFunc KeyFunc[T, K], mergeFunc func(a T, b T) T) []T {
+// ---------------------------------------------------------------------------------------------------------------------
+
+// FoldByKeyFunc 根据key折叠合并切片中的元素，注意折叠完元素在数组中的相对位置可能会被改变，不是稳定的折叠
+func FoldByKeyFunc[T any, K comparable](slice []T, keyFunc KeyFunc[T, K], mergeFunc func(source T, destination T) T) []T {
 	keyMap := make(map[K]T, 0)
 	for index, item := range slice {
 		key := keyFunc(index, item)
 		if lastItem, exists := keyMap[key]; exists {
-			keyMap[key] = mergeFunc(lastItem, item)
+			keyMap[key] = mergeFunc(item, lastItem)
 		} else {
 			keyMap[key] = item
 		}
@@ -102,3 +187,5 @@ func FoldByKey[T any, K comparable](slice []T, keyFunc KeyFunc[T, K], mergeFunc 
 	}
 	return newSlice
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
