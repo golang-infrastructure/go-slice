37c0d8ed800534913e2c8701d59faacb80fd3643
CC11001100 <CC11001100@qq.com>
2022-10-26 00:13:01 +0800

    fill.go & fill_test.go

--- fill.go
+++ fill.go
@@ -2,18 +2,21 @@ package slice
 
 import "math/rand"
 
+// FillSliceValue 为切片中的所有下标赋值同一个值
 func FillSliceValue[T any](slice []T, value T) {
 	for index := range slice {
 		slice[index] = value
 	}
 }
 
+// FillSliceRandomValue 为切片中的所有下标赋值随机值
 func FillSliceRandomValue(slice []int, low, high int) {
 	for index := range slice {
 		slice[index] = rand.Int()%(high-low) + low
 	}
 }
 
+// NewSliceFillRandomValue 创建一个切片，并使用随机值填充
 func NewSliceFillRandomValue(len int, low, high int) []int {
 	slice := make([]int, len)
 	FillSliceRandomValue(slice, low, high)
---
+++ fill_test.go
@@ -0,0 +1,21 @@
+package slice
+
+import (
+	"testing"
+)
+
+func TestFillSliceRandomValue(t *testing.T) {
+	slice := make([]int, 10)
+	FillSliceRandomValue(slice, 100, 0)
+	t.Log(slice)
+}
+
+func TestFillSliceValue(t *testing.T) {
+	slice := make([]int, 10)
+	FillSliceValue(slice, 1)
+	t.Log(slice)
+}
+
+func TestNewSliceFillRandomValue(t *testing.T) {
+	t.Log(NewSliceFillRandomValue(100, 0, 10))
+}
