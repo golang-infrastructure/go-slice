3065d12d35f0af9d6a4ef5620a82ca34598cda03
CC11001100 <CC11001100@qq.com>
2022-10-26 22:20:32 +0800

    prefect to.go

--- to.go
+++ to.go
@@ -21,6 +21,16 @@ func ToStringSlice[T any](slice []T) []string {
 	})
 }
 
+// ToStringSliceByFunc 使用自定义的函数把切片转为string类型的切片
+func ToStringSliceByFunc[T any](slice []T, toStringFunc func(index int, item T) string) []string {
+	stringSlice := make([]string, 0)
+	for index, item := range slice {
+		s := toStringFunc(index, item)
+		stringSlice = append(stringSlice, s)
+	}
+	return stringSlice
+}
+
 // ToString 把整个切片转为一个字符串，通常用于打印、记录之类的
 func ToString[T any](slice []T) string {
 	sb := strings.Builder{}
@@ -32,8 +42,17 @@ func ToString[T any](slice []T) string {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-// ToMap 把切片转为map
-func ToMap[T, K comparable, V any](slice []T, toMapFunc func(index int, item T) (K, V)) map[K]V {
+// ToMap 把切片转为map，将索引作为map的key，将对应下标元素作为map的value
+func ToMap[T, K int](slice []T) map[int]T {
+	resultMap := make(map[int]T, 0)
+	for index, item := range slice {
+		resultMap[index] = item
+	}
+	return resultMap
+}
+
+// ToMapByFunc 把切片转为map，使用自定义的函数从元素中生成key和value
+func ToMapByFunc[T any, K comparable, V any](slice []T, toMapFunc func(index int, item T) (K, V)) map[K]V {
 	resultMap := make(map[K]V, 0)
 	for index, item := range slice {
 		k, v := toMapFunc(index, item)
@@ -53,11 +72,12 @@ func ToSet[T comparable](slice []T) map[T]struct{} {
 	return set
 }
 
-// ToSetWithFunc 把切片转为set
-func ToSetWithFunc[T any, S comparable](slice []T, toSetFunc func(index int, item T) S) map[S]struct{} {
-	set := make(map[S]struct{}, 0)
+// ToSetByFunc 把切片转为set，根据自定义的函数从切片的元素生成set的key
+func ToSetByFunc[T any, K comparable](slice []T, keyFunc KeyFunc[T, K]) map[K]struct{} {
+	set := make(map[K]struct{}, 0)
 	for index, item := range slice {
-		set[toSetFunc(index, item)] = struct{}{}
+		key := keyFunc(index, item)
+		set[key] = struct{}{}
 	}
 	return set
 }
@@ -93,11 +113,6 @@ func ToMatrix[T any](slice []T, columnCount int) [][]T {
 		}
 	}
 	return matrix
-}
-
-// FromMatrix 矩阵转为一维切片
-func FromMatrix[T any](matrix [][]T) []T {
-	return Flat2(matrix)
 }
 
 // ------------------------------------------------ ---------------------------------------------------------------------
